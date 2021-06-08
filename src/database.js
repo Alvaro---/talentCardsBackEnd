0c0df48530bc703779164779128598cf989046c3
Alvaro <alvaro.mercado.valle@gmail.com>
2021-06-08 00:04:26 -0400

    modificacion ruta?

--- src/database.js
+++ src/database.js
@@ -7,15 +7,15 @@ const MongoClient = require('mongodb').MongoClient;
 const URI=process.env.MONGODB_URI 
 ? process.env.MONGODB_URI 
 : 'mongodb://localhost/talent_cards'; */
-
+/* de aca*/
 const URI = "mongodb+srv://alvaroMercado:Alvaro92.1@cluster0.0ozgp.mongodb.net/talent_cards?retryWrites=true&w=majority";
 const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
+  console.log(err)
   client.close();
 });
-
+/*aaaaac */
 const msg_init_db="***** DB Iniciada en "+URI;
 
 mongoose.connect(URI,{
