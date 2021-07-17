4e2651f8506a4eee44de6ff3a55290891927b562
Alvaro <alvaro.mercado.valle@gmail.com>
2021-07-17 01:18:07 -0400

    make it better

--- src/database.js
+++ src/database.js
@@ -3,19 +3,11 @@ const mongoose=require ('mongoose');
 const MongoClient = require('mongodb').MongoClient;
 
 //direccion base
-/*
-const URI=process.env.MONGODB_URI 
-? process.env.MONGODB_URI 
-: 'mongodb://localhost/talent_cards'; */
-/* de aca*/
-const URI = "mongodb+srv://alvaroMercado:Alvaro92.1@cluster0.0ozgp.mongodb.net/talent_cards?retryWrites=true&w=majority";
-const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log(err)
-  client.close();
-});
-/*aaaaac */
+
+const URI=process.env.MONGODB_URL 
+? process.env.MONGODB_URL 
+: 'mongodb://localhost/talent_cards'; 
+
 const msg_init_db="***** DB Iniciada en "+URI;
 
 mongoose.connect(URI,{
